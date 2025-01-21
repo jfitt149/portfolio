0b859861df9e0884b3e03d4c2b1e86ac09160be9
Justin Fitt <jfitt149@gmail.com>
2025-01-21 15:51:29 -0400

    nothing|

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,7 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<Home />} />
         </Routes>
         <Footer />
       </Router>
